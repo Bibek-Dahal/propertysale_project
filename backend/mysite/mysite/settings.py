cb8232be85e9daab9696b46903c35c04efe032fe
Bibek <bibekdahal479@gmail.com>
2022-02-08 22:24:15 +0545

    facebook login added successfully

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -212,15 +212,6 @@ SOCIALACCOUNT_PROVIDERS = {
     },
     'facebook':{
         'SCOPE': ['email', 'public_profile'],
-        'FIELDS': [
-            'first_name',
-            'last_name',
-            'middle_name',
-            'name',
-            'name_format',
-            'picture',
-            'short_name'
-        ],
     }
 }
 
