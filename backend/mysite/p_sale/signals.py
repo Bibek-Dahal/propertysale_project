fff2bd1e5bcc45c44e8ff3ea7354ff28bc49805d
Bibek <bibekdahal479@gmail.com>
2022-02-28 11:45:53 +0545

    tables modified

--- backend/mysite/p_sale/signals.py
+++ backend/mysite/p_sale/signals.py
@@ -3,18 +3,18 @@ from django.dispatch import receiver
 
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
+from .models import KYC
 
-
-# # async_to_sync(channel_layer.group_send)("chat", {"type": "chat.force_disconnect"})
-# @receiver(post_save,sender= KYC)
-# def send_signal(sender,instance,created,**kwargs):
-#     if not created:
-#         print('updated')
-#         channel_layer = get_channel_layer()
-#         async_to_sync(channel_layer.group_send)(f"status_{instance.user.username}", {
-#                 'type': 'status_message',
-#                 'message': instance.status
-#             })
+# async_to_sync(channel_layer.group_send)("chat", {"type": "chat.force_disconnect"})
+@receiver(post_save,sender= KYC)
+def send_signal(sender,instance,created,**kwargs):
+    if not created:
+        print('updated')
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(f"status_{instance.user.username}", {
+                'type': 'status_message',
+                'message': instance.status.kyc_status
+            })
         
         
 
