de5c8d0a45fd2334127855efbadcebd6121ccc27
Bibek <bibekdahal479@gmail.com>
2022-03-01 21:26:33 +0545

    no of views addedd separately for house and lands on websocket connection

--- backend/mysite/p_sale/consumers.py
+++ backend/mysite/p_sale/consumers.py
@@ -1,9 +1,9 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from p_sale.models import Property
+from p_sale.models import House,Land
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class KycStatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['user']
         self.room_group_name = 'status_%s' % self.room_name
@@ -48,7 +48,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         }))
 
 
-class IncreaseViews(AsyncWebsocketConsumer):
+class IncreaseHouseViews(AsyncWebsocketConsumer):
     async def connect(self):
         self.property_id = self.scope['url_route']['kwargs']['id']
         self.group_name = 'property_%d' % self.property_id
@@ -79,7 +79,54 @@ class IncreaseViews(AsyncWebsocketConsumer):
     @database_sync_to_async
     def get_views_count(self,property_id):
         try:
-            pro_obj = Property.objects.get(id = property_id)
+            pro_obj = House.objects.get(id = property_id)
+            pro_obj.no_of_views += 1
+            pro_obj.save()
+            return pro_obj.no_of_views
+        except Exception as e:
+            print('exception called')
+            return None
+
+    async def status_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+class IncreaseLandViews(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.property_id = self.scope['url_route']['kwargs']['id']
+        self.group_name = 'property_%d' % self.property_id
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        
+        await self.accept()
+
+        #send mag to groups after connection
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'status_message',
+                'message': await self.get_views_count(self.property_id)
+            }
+        )
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    @database_sync_to_async
+    def get_views_count(self,property_id):
+        try:
+            pro_obj = Land.objects.get(id = property_id)
             pro_obj.no_of_views += 1
             pro_obj.save()
             return pro_obj.no_of_views
