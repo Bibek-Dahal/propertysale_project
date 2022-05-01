49e262cd4eea03c8425cbf5fbe64b16b04239eed
Bibek <bibekdahal479@gmail.com>
2022-05-01 09:57:14 +0545

    slug added for search field

--- backend/mysite/api/views.py
+++ backend/mysite/api/views.py
@@ -169,7 +169,7 @@ class HouseListView(ListAPIView):
     queryset = House.properties.all()
     serializer_class = HouseSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'longitude','latitude']
+    search_fields = [ 'longitude','latitude','slug']
     # ordering_fields = ['price_in_number']
 
     def get_queryset(self):
@@ -186,7 +186,7 @@ class LandListView(ListAPIView):
     queryset = Land.properties.all()
     serializer_class = LandSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'longitude','latitude']
+    search_fields = [ 'longitude','latitude','slug']
 
     def get_queryset(self):
         q = self.request.query_params.get('ordering')
